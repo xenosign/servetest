c7823e156067daaec044a51fe7401f78862c227f
xenosign <xenosign@naver.com>
2023-05-09 15:58:29 +0900

    express 리액트 빌드 배포

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(express.static(path.join(__dirname, "/build")));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 })
 
 app.listen(app.get("port"), () => {
